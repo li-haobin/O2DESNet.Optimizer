785f90309cbdbe7ead57db9627200c29284ea931
Li Haobin <li_haobin@nus.edu.sg>
2019-08-11 22:31:23 +0800

    Interfaces for ranking and selection

---
+++ O2DESNet.Optimizer/RankingAndSelection/IBestSelector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace O2DESNet.Optimizer
+{
+    public interface IBestSelector<T>
+    {
+        ISingleObjectiveStochasticEvaluator<T> Evaluator { get; }
+        T Select(IList<T> candidates);
+    }
+}
---
+++ O2DESNet.Optimizer/RankingAndSelection/IParetoSelector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace O2DESNet.Optimizer
+{
+    public interface IParetoSelector<T>
+    {
+        IMultiObjectiveStochasticEvaluator<T> Evaluator { get; }
+        T Select(IList<T> candidates);
+    }
+}
